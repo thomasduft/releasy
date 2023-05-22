41c6cb76fd40e48885c81f9511bd6aa376f55d1e
thomasduft <thomas.duft@gmail.com>
2023-05-22 21:16:27 +0200

    syntax sugar

--- src/releasy/Program.cs
+++ src/releasy/Program.cs
@@ -9,9 +9,7 @@ var app = new CommandLineApplication
   Name = "releasy"
 };
 
-const string HelpOption = "-?|-h|--help";
-
-app.HelpOption(HelpOption);
+app.HelpOption();
 
 app.Command("add-changelog", (command) =>
 {
@@ -20,8 +18,7 @@ app.Command("add-changelog", (command) =>
   var prefixOption = command.Option("-p|--prefix", "Prefix like added, changed, deprecated, removed, fixed, security", CommandOptionType.SingleValue);
   var tagOption = command.Option("-t|--tag", "Tag that specifies the changelog entry. Helpful for grouping and such things.", CommandOptionType.SingleValue);
   var messageOption = command.Option("-m|--message", "Changelog text.", CommandOptionType.SingleValue);
-
-  command.HelpOption(HelpOption);
+  command.HelpOption();
   command.OnExecute(() =>
   {
     var issueId = issueIdOption.HasValue()
@@ -58,8 +55,7 @@ app.Command("create-releasenotes", (command) =>
   var inputDirectoryOption = command.Option("-i|--inputDirectory", "Input directory to scan for release note items (defaults to '.')", CommandOptionType.SingleValue);
   var outputDirectoryOption = command.Option("-o|--outputDirectory", "Output directory where a release note will be stored (defaults to '.')", CommandOptionType.SingleValue);
   var archiveDirectoryOption = command.Option("-a|--archiveDir", "Archive directory (if not set deletes changelogs)", CommandOptionType.SingleValue);
-
-  command.HelpOption(HelpOption);
+  command.HelpOption();
   command.OnExecute(() =>
   {
     var version = versionOption.HasValue()
@@ -100,8 +96,7 @@ app.Command("update-changelog", (command) =>
   var permaLinkOption = command.Option("-p|--permaLink", "Permalink template that points to the VCS issue id", CommandOptionType.SingleValue);
   var inputDirectoryOption = command.Option("-i|--inputDirectory", "Input directory to scan for release note items (defaults to '.')", CommandOptionType.SingleValue);
   var changelogFileNameOption = command.Option("-f|--file", "Changelog file to be processed (defaults to 'CHANGELOG.md')", CommandOptionType.SingleValue);
-
-  command.HelpOption(HelpOption);
+  command.HelpOption();
   command.OnExecute(() =>
   {
     var version = versionOption.HasValue()
